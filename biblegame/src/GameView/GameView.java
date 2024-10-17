587fb06ac9cca91ec13cb97457873d8fa7aa9d66
YiCheng612 <cheng277899@gmail.com>
2024-10-17 13:36:48 +0800

    20241017cheng

---
+++ biblegame/src/GameView/GameView.java
@@ -0,0 +1,16 @@
+package GameView;
+
+import Sprite.*;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+public abstract class GameView {
+
+    protected ArrayList<Sprite> elements;
+    protected Door door;
+    protected ImageIcon img;
+
+
+
+}
